01d5b6b8ac151978b6c161739a4df809a81edaa4
Fran Aragón <franaragondeveloper@gmail.com>
2024-11-26 13:58:13 +0100

    feat: add project structure

---
+++ app/exercise1/page.tsx
@@ -0,0 +1,10 @@
+const Exercise1 = () => {
+  return (
+    <div className="container">
+      <h1>Exercise 1: Normal Range</h1>
+      <p>Here will be the Normal Range component.</p>
+    </div>
+  );
+};
+
+export default Exercise1;
