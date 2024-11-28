80966ba2803feec4ebf3c4770531ce0cc94aa857
Fran Aragón <franaragondeveloper@gmail.com>
2024-11-28 15:38:51 +0100

    feat: add Range component and mockAPI

--- app/exercise1/page.tsx
+++ app/exercise1/page.tsx
@@ -1,8 +1,17 @@
-const Exercise1 = () => {
+import { getRangeValues } from '@/services/mockAPI';
+import Range from '@/components/Range';
+
+const Exercise1 = async () => {
+  const { min, max } = await getRangeValues();
+
   return (
     <div className="container">
       <h1>Exercise 1: Normal Range</h1>
-      <p>Here will be the Normal Range component.</p>
+      <p>
+        Range Values: {min} - {max}
+      </p>
+      {/* Pass the min and max values to the Range component */}
+      <Range min={min} max={max} />
     </div>
   );
 };
